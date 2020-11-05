234f60d7beabd920f0d435c411be505952aefbb1
jnbofori <jnbofori@gmail.com>
2020-11-05 18:49:37 +0000

    key gone

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^+ah%f2!p4)q$fqd48e6pq(zmv1f6k453_d7fz-9lb!j%nox&-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
